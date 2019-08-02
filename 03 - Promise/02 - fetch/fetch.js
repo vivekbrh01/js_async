df88f5a8a06921dbfa780c31483e27c9eebeab46
nnnkit <iankit17@gmail.com>
2019-08-02 13:03:36 +0530

    update fetch exercise

--- 03 - Promise/02 - fetch/fetch.js
+++ 03 - Promise/02 - fetch/fetch.js
@@ -1,1 +1,9 @@
 // Using XMLHttpRequest and Promise write your own implementation of browesrs fetch method.
+// 1. It takes two parameter 'url' and type of request (GET | POST)
+// 2. Returns a promise
+// 3. Resolve the promise when data is fetched (onload)
+// 4. Reject the promise when error occured (onerror)
+
+function fetchData() {
+  // your code goes here
+}
