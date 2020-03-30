9f2fa69da9e9a7e54d4f89860a17d562069914e8
Vivek Baruah <vivekbrh01@gmail.com>
2020-03-30 20:11:06 +0530

    git user using xhr

--- 02 - XMLHttpRequest/github-user/main.js
+++ 02 - XMLHttpRequest/github-user/main.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+document.querySelector('.btn').addEventListener('click', findUserName);
+
+
+function findUserName(element) {
+    let input = document.querySelector('.input').value;
+
+    function getInfo (user) {
+
+        document.querySelector('.user-img').src = user.avatar_url;
+        document.querySelector('.user-name').innerText = `Name: ${user.name}`;
+        document.querySelector('.user-id').innerText = `Id: ${user.id}`;
+        document.querySelector('.user-bio').innerText = `Bio: ${user.bio}`; 
+
+    }
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET',`https://api.github.com/users/${input}`, true);
+    xhr.onload = function() {
+
+        if (this.status === 200) {
+            console.log( JSON.parse(xhr.response) );
+            const response= JSON.parse(this.response);
+            getInfo(response);
+        }
+    }
+
+    xhr.send();
+    element.preventDefault();
+}
